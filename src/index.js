54f45fdb94453670d896271f469fc245728d7fe7
Dan Fletcher <dannyjfletcher@outlook.com>
2018-05-24 12:37:49 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,57 @@
+function returnModels(models) {
+    return models.length > 1 ? models : models[0]
+}
+
+let FactoryModels = {}
+let _store = {}
+
+export default  (store = {}) => {
+    _store = store
+    return {
+        define (models) {
+            FactoryModels = models
+            return this
+        },
+        build () {
+            return (model = '', count = 1) => {
+                return {
+                    make (properties = {}) {
+                        let models = []
+
+                        for (let i = 0; i < count; i++) {
+                            models.push({...FactoryModels[model], ...properties})
+                        }
+
+                        return returnModels(models)
+                    },
+
+                    create (...args) {
+                        if (typeof args[0] === 'function') {
+                            let callback = args[0]
+                            let models = []
+
+                            for (let i = 0; i < count; i++) {
+                                let newModel = FactoryModels[model]
+                                newModel = callback(newModel)
+                                models.push(newModel)
+                                // _store.commit(`add${model}`, model)
+                            }
+
+                            return returnModels(models)
+                        }
+                        let properties = args[0] ? args[0] : {}
+                        let models = []
+
+                        for (let i = 0; i < count; i++) {
+                            let model = this.make(1, properties)
+                            models.push(model)
+                            // _store.commit(`add${model}`, model)
+                        }
+
+                        return returnModels(models)
+                    },
+                }
+            }
+        }
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+/* global __dirname, require, module*/
+
+const webpack = require('webpack');
+const path = require('path');
+const env = require('yargs').argv.env; // use --env with webpack 2
+const pkg = require('./package.json');
+const ObjectRestSpreadPlugin = require('@sucrase/webpack-object-rest-spread-plugin');
+
+let libraryName = pkg.name;
+
+let outputFile, buildMode;
+
+if (env === 'build') {
+  buildMode = 'production'
+  outputFile = libraryName + '.min.js';
+} else {
+  buildMode = 'development'
+  outputFile = libraryName + '.js';
+}
+
+const config = {
+  mode: buildMode,
+  optimization: { minimize: buildMode === 'production' ? true : false },
+  entry: __dirname + '/src/index.js',
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/lib',
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/
+      },
+    ]
+  },
+  resolve: {
+    modules: [path.resolve('./node_modules'), path.resolve('./src')],
+    extensions: ['.json', '.js']
+  },
+  plugins: [
+  ]
+};
+
+module.exports = config;
